83b03c2075622c32944b36b2a6e298fb341de010
MrB <mrblasco@gmail.com>
2025-11-24 18:45:15 +0100

    gen os final data

--- scripts/gen_os_final_rds.R
+++ scripts/gen_os_final_rds.R
@@ -1,21 +1,30 @@
+# 
 library(dplyr)
 library(yaml)
 
 config <- yaml.load_file("config.yml")
 data_dir <- config$data_dir
 
-atyp <- readRDS("data/processed/atyp.rds")
+data_path <- file.path(data_dir, "rds/final.rds")
+atyp_path <- file.path("data", "processed", "atyp.rds")
+code_path <- file.path("data", "processed", "class_code.rds")
 
-filename_data <- file.path(data_dir, "rds/final.rds")
+# Load data 
+atyp <- readRDS(atyp_path)
+code <- readRDS(code_path)
+ds <- readRDS(data_path) 
 
-ds <- readRDS(filename_data) %>%
+# Process and filter data
+ds <- ds %>%
     dplyr::filter(nchar(team) < 3, year > 1999) %>% 
     dplyr::mutate(
         total_authors = female_authors + male_authors,
     )
 
+
 ds_matched <- ds %>% 
-    left_join(atyp, by = c("id"="ID"))
+    left_join(atyp, by = c("id"="ID")) %>% 
+    left_join(code, by = "id")
 
 outpath <- file.path("data", "processed", "os_final.rds")
 saveRDS(ds_matched, outpath)
