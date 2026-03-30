90cf22890a1304b3e9ee64ff14d0637fb0322b21
Andrea Blasco <mrb@Andreas-Mac-Studio.local>
2026-03-30 19:00:09 +0200

    render script

--- scripts/10_render_manuscript.R
+++ scripts/10_render_manuscript.R
@@ -1,11 +1,14 @@
 library(rmarkdown)
 
-
 input_file <- file.path("manuscript", "_main.Rmd")
 
+timestamp <- format(Sys.time(), "%Y-%m-%d")
+
 out <- render(
-    input_file,
-    output_format = "bookdown::pdf_document2"
+  input_file,
+  output_format = "bookdown::pdf_document2",
+  output_file = "gender_teaching_syllabi.pdf",
+  output_dir = file.path("submission", timestamp)
 )
 
 message("Rendered ", out)
