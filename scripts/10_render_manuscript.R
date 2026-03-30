462c1275b53875ae8e2d641694651e2c17c1a653
Andrea Blasco <mrb@Andreas-Mac-Studio.local>
2026-03-30 12:19:49 +0200

    edit render manu

--- scripts/10_render_manuscript.R
+++ scripts/10_render_manuscript.R
@@ -1,11 +1,24 @@
 library(rmarkdown)
-
+library(bookdown)
 
 input_file <- file.path("manuscript", "_main.Rmd")
 
+output_dir <- file.path("manuscript", "build")
+version_tag <- format(Sys.Date(), "%Y-%m-%d")
+tex_file <- file.path(output_dir, paste0("manuscript_", version_tag, ".tex"))
+pdf_file <- file.path(output_dir, paste0("manuscript_", version_tag, ".pdf"))
+
 out <- render(
-    input_file,
-    output_format = "bookdown::pdf_document2"
+    input = input_file,
+    output_format = "bookdown::pdf_document2",
+    output_file = basename(pdf_file),
+    output_dir = output_dir,
+    clean = FALSE,
+    envir = new.env()
 )
 
-message("Rendered ", out)
+generated_tex <- sub(".pdf$", ".tex", out)
+file.rename(generated_tex, tex_file)
+
+message("Rendered PDF: ", out)
+message("Saved TeX: ", tex_file)
