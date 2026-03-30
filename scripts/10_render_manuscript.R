64e4687e62f04ac8588964f4da4fe2120cace1de
Andrea Blasco <mrb@Andreas-Mac-Studio.local>
2026-03-30 19:13:40 +0200

    edit render manu

--- scripts/10_render_manuscript.R
+++ scripts/10_render_manuscript.R
@@ -3,12 +3,14 @@ library(rmarkdown)
 input_file <- file.path("manuscript", "_main.Rmd")
 
 timestamp <- format(Sys.time(), "%Y-%m-%d")
+branch <- system("git rev-parse --abbrev-ref HEAD", intern = TRUE)
+branch <- gsub("/", "-", branch)
 
 out <- render(
   input_file,
   output_format = "bookdown::pdf_document2",
   output_file = "gender_teaching_syllabi.pdf",
-  output_dir = file.path("submission", timestamp)
+  output_dir = file.path("submission", branch)
 )
 
 message("Rendered ", out)
